274f11a7eba08aa7c5421860ae556e8e3f8b2b74
Don <leghorncorn@gmail.com>
2014-08-15 23:12:27 -0400

    Added CRUD functionality

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
 
   get 'hello_angular/show'
 
+  resources :pages
+  root 'pages#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
